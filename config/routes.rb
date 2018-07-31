f7318ae29d2c7199bbbd21200b2889c6269a43b5
LUTlp <lutlp@outlook.com>
2018-07-31 18:42:44 +0200

    maj

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
-  get 'users/new', to: 'users#new', as: 'nouvelutilisateur'
-  post 'users/new', to: 'users#create', as: 'creationutilisateur'
+  get '/users/new', to: 'users#new', as: 'nouvelutilisateur'
+  post '/users/new', to: 'users#create', as: 'creationutilisateur'
   get '/users/results', to: 'users#results'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
