39374479f331984e57a853c22f23cd21dcc3ac5c
LUTlp <lutlp@outlook.com>
2018-07-31 18:48:41 +0200

    maj

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
-
     @user = User.new
     @user.username = params[:user][:username]
     @user.email = params[:user][:email]
