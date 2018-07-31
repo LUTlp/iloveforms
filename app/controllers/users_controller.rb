664ab3088473c4fa88af12819bc46b1e1eaec4dc
LUTlp <lutlp@outlook.com>
2018-07-31 18:20:37 +0200

    3ème form ok

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.new
+    @user.username = params[:user][:username]
+    @user.email = params[:user][:email]
+    @user.bio = params[:user][:bio]
+    @user.save
+    redirect_to users_results_path
+  end
+
+  def results
+  end
+
+end
