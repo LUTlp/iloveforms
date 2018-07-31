bc670f09d75453d7d0b14c4b0d818569b3fee328
LUTlp <lutlp@outlook.com>
2018-07-31 23:23:58 +0200

    "separation en plusieurs pages"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,35 @@
 class UsersController < ApplicationController
-  def new
+
+
+  def newformhtml
+  end
+
+  def createformhtml
+    @user = User.new
+    @user.username = params[:username]
+    @user.email = params[:email]
+    @user.bio = params[:bio]
+    @user.save
+    redirect_to users_results_path
+  end
+
+  def newformtag
     @user = User.new
   end
 
-  def create
+  def createformtag
+    @user = User.new
+    @user.username = params[:username]
+    @user.email = params[:email]
+    @user.bio = params[:bio]
+    @user.save
+    redirect_to users_results_path
+  end
+
+  def newformfor
+    @user = User.new
+  end
+  def createformfor
     @user = User.new
     @user.username = params[:user][:username]
     @user.email = params[:user][:email]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
-  get '/users/new', to: 'users#new', as: 'nouvelutilisateur'
-  post '/users/new', to: 'users#create', as: 'creationutilisateur'
+  get '/users/newformhtml', to: 'users#newformhtml', as: 'nouvelutilisateurhtml'
+  post '/users/newformhtml', to: 'users#createformhtml'
+
+  get '/users/newformtag', to: 'users#newformtag', as: 'nouvelutilisateurformtag'
+  post '/new', to: 'users#createformtag', as: 'creationutilisateur'#pour le 2ème formulaire
+
+  get '/users/newformfor', to: 'users#newformfor', as: 'nouvelutilisateurformfor'
+  post '/users/newformfor', to: 'users#createformfor'
+
   get '/users/results', to: 'users#results'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
